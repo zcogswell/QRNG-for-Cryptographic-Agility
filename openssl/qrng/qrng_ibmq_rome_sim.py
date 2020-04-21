67bd13809e1614d32f09a0efac1d47ca852bf38f
gdk7676 <geoffreykanteles@gmail.com>
2020-04-21 16:08:19 -0400

    Test python scripts to call ibmq hardware etc.
    Scripts either call IBMQ hardware w/ API token or use local qiskit backend simulator. Currently print output to evaluate speed, can just return output as needed.

---
+++ openssl/qrng/qrng_ibmq_rome_sim.py
@@ -0,0 +1,8 @@
+import qrng
+
+qrng.set_provider_as_IBMQ('8b6a87f834979265ce5b69af4fffa759f76a592250c8b24f227e7642801b901bc1c9bb7b92bbff9594cced1453193b1ff0f7c107817eb7112b5eaa9c63332b27')
+qrng.set_backend('ibmq_rome')
+
+for i in range(10):
+	num = qrng.get_random_int32()
+	print(num)
