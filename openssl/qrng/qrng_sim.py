7f843a1bebc4ad50ff504fb6ef46f65e2026df76
Zack Cogswell <zachary.s.cogswell@gmail.com>
2020-04-21 20:31:58 -0700

    Documented qrng_sim.py, created script to demonstrate RDRAND, removed libdrng.

--- openssl/qrng/qrng_sim.py
+++ openssl/qrng/qrng_sim.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+"""Runs qrng local simulator
+"""
+__author__ = "Goeffrey Kanteles, Zack Cogswell"
+
 from time import time_ns
 from argparse import ArgumentParser
 
@@ -6,24 +10,36 @@ from qrng import get_random_int32, set_backend, set_provider_as_IBMQ
 
 
 def parse_args():
-	parser = ArgumentParser()
-	parser.add_argument('-t', '--timer', action='store_true', help='Time the first random generation.')
-	return parser.parse_args()
+    """Parses command line arguments
+
+    Returns:
+        Namespace -- arguments and their values
+    """
+    parser = ArgumentParser()
+    parser.add_argument('-t', '--timer', action='store_true', \
+            help='Time the first random generation.')
+    return parser.parse_args()
+
+def main():
+    """Uses the qrng library's local simulator to generate random 32b ints.
+        The ints are stored in a local file called rand.txt
+    """
+    timer_flag = parse_args().timer
+    set_provider_as_IBMQ('')
+    set_backend()
+    if timer_flag:
+        time1 = time_ns()
+        num = get_random_int32()
+        num = num.to_bytes(4, 'big')
+        with open('rand.txt', 'wb+') as outfile:
+            outfile.write(num)
+        time2 = time_ns()
+        print('{0}us'.format((time2 - time1)/1000))
+    while True:
+        num = get_random_int32()
+        num = num.to_bytes(4, 'big')
+        with open('rand.txt', 'wb+') as outfile:
+            outfile.write(num)
 
 if __name__ == '__main__':
-	timer_flag = parse_args().timer
-	set_provider_as_IBMQ('')
-	set_backend()
-	if timer_flag:
-		time1 = time_ns()
-		num = get_random_int32()
-		num = num.to_bytes(4, 'big')
-		with open('rand.txt', 'wb+') as f:
-			f.write(num)
-		time2 = time_ns()
-		print('{0}us'.format((time2 - time1)/1000))
-	while True:
-		num = get_random_int32()
-		num = num.to_bytes(4, 'big')
-		with open('rand.txt', 'wb+') as f:
-			f.write(num)
+    main()
--- openssl/trng/README.md
+++ openssl/trng/README.md
@@ -1,3 +1,5 @@
 ## TRNG
 A true random number generator.
 Currently using the TRNG found inside modern AMD64 processors.
+
+OpenSSL comes with an RDRAND Engine.
