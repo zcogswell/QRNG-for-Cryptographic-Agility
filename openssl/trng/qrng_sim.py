a4f9acdabbe4271d7e8346daea8188eb755d2d22
gdk7676 <geoffreykanteles@gmail.com>
2020-04-24 22:21:41 -0400

    added qrng smallcrush

--- openssl/trng/qrng_sim.py
+++ openssl/trng/qrng_sim.py
@@ -25,8 +25,10 @@ def main():
         The ints are stored in a local file called rand.txt
     """
     timer_flag = parse_args().timer
-    qrng.set_provider_as_IBMQ('8b6a87f834979265ce5b69af4fffa759f76a592250c8b24f227e7642801b901bc1c9bb7b92bbff9594cced1453193b1ff0f7c107817eb7112b5eaa9c63332b27')
-    qrng.set_backend('ibmq_qasm_simulator')
+    #qrng.set_provider_as_IBMQ('8b6a87f834979265ce5b69af4fffa759f76a592250c8b24f227e7642801b901bc1c9bb7b92bbff9594cced1453193b1ff0f7c107817eb7112b5eaa9c63332b27')
+    #qrng.set_backend('ibmq_qasm_simulator')
+    qrng.set_provider_as_IBMQ('')
+    qrng.set_backend()
     if timer_flag:
         time1 = time_ns()
         num = qrng.get_random_int32()
